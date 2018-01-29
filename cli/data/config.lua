c26e081b96fecdf307f8f9efb5e39f21c9b41eff
best98ir <hphs5050@gmail.com>
2018-01-29 19:25:05 +0330

    Add files via upload

---
+++ cli/data/config.lua
@@ -0,0 +1,40 @@
+do local _ = {
+  admins = {
+    {
+      434418436,
+      "@sasan_programmer"
+    }
+  },
+  disabled_channels = {},
+  enabled_plugins = {
+    "MRCore",
+    "Msg",
+    "BanHammer",
+    "Administrative",
+    "Help",
+    "Limitmember",
+    "Rank",
+    "Openchat",
+    "Del-Chat",
+    "Monshi",
+    "Rank",
+    "Warn",
+    "Auto-Lock",
+    "Plugins",
+    "Fuck",
+    "cleanmembers",
+    "Zarinpal",
+    "Fun",
+    "addkick"
+  },
+  moderation = {
+    data = "./data/moderation.json",
+    tabgood = "./data/tabgood.json"
+  },
+  sudo_users = {
+    434418436,
+    490443765
+  }
+}
+return _
+end
